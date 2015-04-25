a58041f26ad8509231f58ea9f904e98173ed5baf
wrcj <wrcj12138@163.com>
2015-04-25 08:45:48 +0800

    Initial Commit

---
+++ 传感器Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  传感器Demo
+//
+//  Created by zjsruxxxy3 on 15/4/25.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 传感器Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  传感器Demo
+//
+//  Created by zjsruxxxy3 on 15/4/25.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
